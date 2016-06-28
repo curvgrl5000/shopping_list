7d3e7a72c1d28b6055240d27bef0a86b98897293
anna mendoza <anna@curvecommunications.biz>
2016-06-28 00:05:32 -0700

    tweaking the counter for the dynamic list

--- js/shopping.js
+++ js/shopping.js
@@ -1,15 +1,31 @@
 $(document).ready(function() {
 
-	$(".shopping_list").on("click", ".cbutton", function( event ) {
-		event.preventDefault();
-		var itemToAdd = $("input#shopping_items").val();
-		 if  ( itemToAdd == "" ) {
-   		 	alert("Please add an Item!");
-  		}
-	});
+      // var count = 0;
+      var count = $("li").length;
+  $(".shopping_list").on("click", ".cbutton", function( event ) {
+    event.preventDefault();
+    var addItem = $("input#shopping_items").val();
+
+     if  ( addItem != "" ) {
+        $(".listed_items").append(
+               '<li><input id=\"cb'+count+ '"name=\"cb'+count+ '"type=\"checkbox\" ><label for=\"cb'+count+ '">' 
+               +addItem+ '</label><button class=\"cbutton cbutton--effect-jelena right\"><i class=\"cbutton__icon fa fa-fw fa-trash-o\"></i><span class=\"cbutton__text\">delete</span></button></li>');
+        count++;
+        $(this).parent().children("input").val(""); 
+      } else {
+        alert("Please add an Item!");
+      };
+  });
 
 
 });
+
+
+
+
+
+
+
 
 
 
