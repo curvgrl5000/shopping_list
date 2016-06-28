0f3186acf136aa76eecbb1652082157682a92e68
anna mendoza <anna@curvecommunications.biz>
2016-06-27 20:02:23 -0700

    javascript experiments

--- js/shopping.js
+++ js/shopping.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	$("#shopping_items input").submit( function() 
 	{
-		if(   $(this).val().length === 0 ) {  
+		if( $(this).val().length === 0 ) {  
 			alert("Sorry the field can't be blank!");
-			event.preventDefault();
 		}
 		else
 		{
 		console.log("Everything is okay");
+		};
 	});
 });
