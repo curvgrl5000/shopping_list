0f42f63ff5e3549f5e9cc451ec9dd204465ee382
anna mendoza <anna@curvecommunications.biz>
2016-07-12 02:53:12 -0700

    fixed all things except the dynamic svg load, now need to get the counter going and then the add to favorite list or a dropdown

--- js/shopping.js
+++ js/shopping.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   var count = $("li").length + 1;
+
   $(".shopping_list").on("click", ".cb_add", function( event ) {
     event.preventDefault();
     var addItem = $("input#shopping_items").val();
@@ -7,26 +8,23 @@ $(document).ready(function() {
      if  ( addItem != "" ) {
         $(".listed_items").append(
                '<li><input id=\"cb'+count+ '"name=\"cb'+count+ '"type=\"checkbox\" ><label for=\"cb'+count+ '">' 
-               +addItem+ '</label><button class=\"cbutton cbutton--effect-jelena right\"><i class=\"cbutton__icon fa fa-fw fa-trash-o\"></i><span class=\"cbutton__text\">delete</span></button></li>');
+               +addItem+ '</label><button class=\"cbutton cbutton--effect-jelena right\" type=\"button\" onclick=\"jRem()\"><i class=\"cbutton__icon fa fa-fw fa-trash-o\"></i></button></li>');
         count++;
         $(this).parent().children("input").val(""); 
       } else {
         alert("Please add an Item!");
-      };
+      }
   });
 });
 
 
+// A SIMPLER WAY
+function jAdd() {
+    $('#listed_itemss').append('<li><input type="checkbox" /> item</li>');
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+function jRem() {
+    $('.listed_items').children().filter(function () {
+        return this.firstChild.checked;
+    }).remove();
+}
