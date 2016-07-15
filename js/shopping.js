1596b63e664d478f620a06c05fb5627ecb47d456
anna mendoza <anna@curvecommunications.biz>
2016-07-15 12:35:16 -0700

    addding a local storage function to get and set the a completed list

--- js/shopping.js
+++ js/shopping.js
@@ -12,7 +12,8 @@ $(document).ready(function() {
                +addItem+ '</label><button class="cbutton cbutton--effect-jelena right" type="button" onclick="jRemOne(\'cb' + count + ' \')"><i class="cbutton__icon fa fa-fw fa-trash-o"></i></button></li> ' );
         count++;
         $(this).parent().children("input").val("");
-        dynamicAnimation(); 
+        dynamicAnimation();
+       // localStorage.setItem(addItem);
       } else {
         alert("Please add an Item!");
       }
@@ -34,19 +35,62 @@ function jRemOne(current_count) {
   console.log($(this));  
 }
 
+function completedTasks() {
+  var addItem = $("input#shopping_items").val();
+  var id = $("#cb")
+  localStorage.setItem(id, addItem);
+  var value = localStorage.getItem(id, addItem);
+  $("#completed").append(
+    '<li> Great job, you got ' + value + ' done!</li>'
+    );
+}
+
 ////////////////////////////////////////////////////////////////////
 
 // A SIMPLER WAY for test case button
-function jAddd() {
+function jAddThis() {
     $('#listed_itemss').append('<li class="test"><input type=\"checkbox\" /><label> item</label><button class=\"cbutton cbutton--effect-jelena right\" type=\"button\" onclick=\"jRemOne()\"><i class=\"cbutton__icon fa fa-fw fa-trash-o\"></i></button></li></li>');
 }
 
 //REMOVE ALL OF THE LISTED ITEMS for test case button
-function jRemm() {
+function jRemAll() {
     $('#listed_itemss').children().filter(function () {
         return this.firstChild.checked;
     }).remove();
 }
+
+////////////////////////////////////////////////////////////////////
+// LOCA STORAGE TESTING
+function storageAvailable(type) {
+  try {
+    var storage = window[type],
+      x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  }
+  catch(e) {
+    return false;
+  }
+}
+
+if (storageAvailable('localStorage')) {
+  console.log("Yippee! We can use localStorage awesomeness")
+}
+else {
+  console.log("Too bad, no localStorage for us")
+}
+
+
+if (storageAvailable('sessionStorage')) {
+  console.log("Yippee! We can use sessionStorage awesomeness")
+}
+else {
+  console.log("Too bad, no localStorage for us")
+}
+
+////////////////////////////////////////////////////////////////////
+// TESTING IF STORAGE HAS BEEN POPULATED
 
 
 
