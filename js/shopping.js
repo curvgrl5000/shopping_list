b84350f7f059190faa40ebb3b166aebb3bb0e81c
anna mendoza <anna@curvecommunications.biz>
2016-07-20 13:12:50 -0700

    adding the functionality to the done array, with the  appending to the done .done

--- js/shopping.js
+++ js/shopping.js
@@ -7,7 +7,7 @@ var shoppingList = (function($) {
 
   $(function () {
     $item_list.todo = $(".listed_items");
-    $item_list.done = $(".completed");
+    $item_list.done = $("#completed");
     $input_field = $('.input_field').children("input");
   });
 
@@ -17,7 +17,7 @@ var shoppingList = (function($) {
     }
 
     var $list_item = $('<li><input type="checkbox"><label>'
-      + '</label><button class="cbutton cbutton--effect-jelena right" type="button"><i class="cbutton__icon fa fa-fw fa-trash-o"></i></button></li> ' );
+      + '</label><button class="cbutton cbutton--effect-jelena right" type="button"><i class="cbutton__icon fa fa-fw fa-check"></i></button></li> ' );
     
     function createItem(item_name) {
       var id = uid(),
@@ -28,8 +28,9 @@ var shoppingList = (function($) {
         $current_item.find('label').text(item_name);
 
         $current_item.find('button').on('click', function () {
-          jRemOne($current_item);
-          completedTasks($current_item);
+          if  ($('input[type=checkbox]',$current_item).is( ':checked' ))  {  
+            $current_item.appendTo($item_list.done);   //appendTo implicitly removes the DOm Object from its current place; 
+          }
         });
         return $current_item;
     }
@@ -45,11 +46,8 @@ var shoppingList = (function($) {
         $item_list[to];
       }
     }
-    shoppingList.move(item_name, 'todo', 'done');
 
 })($); // immediate function
-
-// shoppingList.move($("input#shopping_items").val(), 'todo', 'done');  Nic this worked too!!
 
 $(document).ready(function() {
   ['Milk','Trash Bag', 'Water', 'Liquid', 'Hair Stuff'].forEach(shoppingList.addItem);
@@ -65,76 +63,45 @@ $(document).ready(function() {
 
 // REMOVE ALL OF THE LISTED ITEMS
 function jRem() {
-    $('.listed_items').children().filter(function () {
-        return this.firstChild.checked;
-    }).remove();
+    $('.listed_items > li').has(":checked").remove(); 
 }
 
-// REMOVE ONE OF THE LISTED ITEMS
-function jRemOne(item) {
-  if  ($('input[type=checkbox]',item).is( ':checked' ))  {
-    item.remove();
-  }
-}
-
-// ADD THE LOCALLY STORED ITEMS TO A DONE LIST
-function completedTasks(item) {
-  if  ($('input[type=checkbox]',item).is( ':checked' ))  {
-    var completedItem = shoppingList.done;
-    var id = $("#cb");
-    localStorage.setItem(id, completedItem);
-    var value = localStorage.getItem(id, completedItem);
-    $("#completed").append(
-      '<li class="done"><label>'+ value + '</label></li>'
-    );
-  };
-  console.log(completedItem);
-
-}
+// REMOVE ONE OF THE LISTED ITEMS //  A function call
+// function jRemOne(item) {
+//   if  ($('input[type=checkbox]',item).is( ':checked' ))  {
+//     item.remove();
+//   }
+// }
 
 ////////////////////////////////////////////////////////////////////
+// LOCAL STORAGE TESTING
+// function storageAvailable(type) {
+//   try {
+//     var storage = window[type],
+//       x = '__storage_test__';
+//     storage.setItem(x, x);
+//     storage.removeItem(x);
+//     return true;
+//   }
+//   catch(e) {
+//     return false;
+//   }
+// }
 
-// A SIMPLER WAY for test case button
-function jAddThis() {
-    $('#listed_itemss').append('<li class="test"><input type=\"checkbox\" /><label> item</label><button class=\"cbutton cbutton--effect-jelena right\" type=\"button\" onclick=\"jRemOne()\"><i class=\"cbutton__icon fa fa-fw fa-trash-o\"></i></button></li></li>');
-}
-
-//REMOVE ALL OF THE LISTED ITEMS for test case button
-function jRemAll() {
-    $('#listed_itemss').children().filter(function () {
-        return this.firstChild.checked;
-    }).remove();
-}
-
-////////////////////////////////////////////////////////////////////
-// LOCA STORAGE TESTING
-function storageAvailable(type) {
-  try {
-    var storage = window[type],
-      x = '__storage_test__';
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
-  }
-  catch(e) {
-    return false;
-  }
-}
-
-if (storageAvailable('localStorage')) {
-  console.log("Yippee! We can use localStorage awesomeness")
-}
-else {
-  console.log("Too bad, no localStorage for us")
-}
+// if (storageAvailable('localStorage')) {
+//   console.log("Yippee! We can use localStorage awesomeness")
+// }
+// else {
+//   console.log("Too bad, no localStorage for us")
+// }
 
 
-if (storageAvailable('sessionStorage')) {
-  console.log("Yippee! We can use sessionStorage awesomeness")
-}
-else {
-  console.log("Too bad, no localStorage for us")
-}
+// if (storageAvailable('sessionStorage')) {
+//   console.log("Yippee! We can use sessionStorage awesomeness")
+// }
+// else {
+//   console.log("Too bad, no localStorage for us")
+// }
 
 
 
